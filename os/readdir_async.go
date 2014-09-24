9339cb1443dd83107942cc7567aff4cfa0a4738c
Jim Cheung <usr.jim@gmail.com>
2014-09-25 02:21:25 +0800

    added readdir using goroutine

---
+++ os/readdir_async.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sync"
+)
+
+var deep int
+var wg sync.WaitGroup
+
+func main() {
+	target := "/tmp"
+	wg.Add(1)
+	go dig(target)
+	wg.Wait()
+}
+
+func dig(p string) {
+	dh, _ := os.Open(p)
+	defer dh.Close()
+	defer wg.Done()
+
+	f, _ := dh.Readdir(-1)
+	for _, fi := range f {
+		if fi.IsDir() {
+			child := p + string(os.PathSeparator) + fi.Name()
+			wg.Add(1)
+			go dig(child)
+		} else {
+			fmt.Println(p + fi.Name())
+		}
+	}
+}
